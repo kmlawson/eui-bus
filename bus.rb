bea5877bffd0419fb9c1b62a46422f2c2fd9724b
K. M. Lawson <regs@huginn.net>
2013-02-24 22:41:25 +0100

    added options

--- bus.rb
+++ bus.rb
@@ -21,14 +21,33 @@ else
 end
 
 a=Mechanize.new { |agent| agent.user_agent_alias='Mac Safari'}   
-page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-1/stop-salviati-fs/#{daystring}/timetables.aspx?date=24%2f02&DescrFermata=SALVIATI+FS&Fermata=FM1926&idC=180&idO=0&Linea=1&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=d")
-mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
-
+def getpage(whichstop)
+	if whichstop=='flats'
+		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-1/stop-salviati-fs/#{daystring}/timetables.aspx?date=24%2f02&DescrFermata=SALVIATI+FS&Fermata=FM1926&idC=180&idO=0&Linea=1&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=d")
+		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
+	elsif whichstop=='sdsouth'
+		mytable=nil
+	elsif whichstop=='sdnorth'
+		mytable=nil
+	elsif whichstop=='sm7'
+		mytable=nil
+	elsif whichstop=='sm1a'
+		mytable=nil
+	elsif whichstop=='sm1b'
+		mytable=nil
+	elsif whichstop=='smn1a'
+		mytable=nil
+	elsif whichstop=='smn1b'
+		mytable=nil
+	else
+		mytable=nil
+	end
+end
 get '/' do
 	"<h1>Busses Near EUI</h1><br /><br />
 	<a href='/flats'>Bus 1A From Salviati to Santa Maria Novella</a>"
 end
 
-get '/flats' do
-	mytable
+get '/*' do
+	"You passed #{params[:splat]}"
 end
