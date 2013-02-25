41165de1147e0589c0ca2fce0bfb3e8ad873c274
K. M. Lawson <regs@huginn.net>
2013-02-25 07:20:10 -0800

    Fixed filter

--- bus.rb
+++ bus.rb
@@ -40,8 +40,8 @@ def getpage(whichstop,a)
 		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
 		return mytable
 	elsif whichstop=='sdnorth'
-		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-7/stop-san-domenico-01---european-university/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=SAN+DOMENICO+01+-+EUROPEAN+UNIVERSITY&Fermata=FM0370&idC=180&idO=0&Linea=7&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=a")
-		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
+		page=a.get("http://www.ataf.net/it/orari-e-percorsi/orari-e-linee/line-7/stop-san-domenico-01---european-university/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=SAN+DOMENICO+01+-+EUROPEAN+UNIVERSITY&Fermata=FM0370&idC=180&idO=0&Linea=7&LN=it-IT&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=a")
+		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioATrasposto"]'
 		return mytable
 	elsif whichstop=='fiesole'
 		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-7/stop-fiesole---vinandro-osteria/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=FIESOLE+-+VINANDRO+OSTERIA&Fermata=FM0375&idC=180&idO=0&Linea=7&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=d")
@@ -49,15 +49,15 @@ def getpage(whichstop,a)
 		return mytable
 	elsif whichstop=='sm1'
 		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-1/stop-san-marco---gran-caffe--san-marco/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=SAN+MARCO+-+GRAN+CAFFE%27+SAN+MARCO&Fermata=FM0084&idC=180&idO=0&Linea=1&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=a")
-		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
+		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioATrasposto"]'
 		return mytable
 	elsif whichstop=='sm7'
 		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-7/stop-la-pira/#{daystring}/timetables.aspx?date=25%2f02&dateHash=078d5c8334e82266787a17aae62fa357&DescrFermata=LA+PIRA&Fermata=FM1884&idC=180&idO=0&Linea=7&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=a")
-		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
+		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioATrasposto"]'
 		return mytable
 	elsif whichstop=='smn'
-		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-1/stop-stazione-deposito-bagagli/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=STAZIONE+DEPOSITO+BAGAGLI&Fermata=FM0019&idC=180&idO=0&Linea=1&LN=en-US&Orario=#{orario}&#Servizio=#{servizio}&Tmp=0&Verso=a")
-		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioDTrasposto"]'
+		page=a.get("http://www.ataf.net/en/timetables-and-routes/timetables-and-routes/line-1/stop-stazione-deposito-bagagli/#{daystring}/timetables.aspx?date=25%2f02&DescrFermata=STAZIONE+DEPOSITO+BAGAGLI&Fermata=FM0019&idC=180&idO=0&Linea=1&LN=en-US&Orario=#{orario}&Servizio=#{servizio}&Tmp=0&Verso=a")
+		mytable=page.search '//*[@id="ctl00_ContentPlaceHolderMain_GridViewOrarioATrasposto"]'
 		return mytable
 	else
 		mytable=nil
@@ -66,8 +66,7 @@ end
 set :raise_errors => true
 set :show_exceptions => true
 get '/' do
-	"
-	<!doctype html>
+	"<!doctype html>
 	<html lang='en'>
 	<head>
 		<meta charset='utf-8'>
@@ -78,33 +77,33 @@ get '/' do
 		  <![endif]-->
 	</head>
 <body>
-	<h1>Busses Near EUI</h1><br /><br />
+	<h1>Busses Near EUI</h1>
 	<h2>San Domenico</h2>
 	<p>
-		<a href='/sdsouth'>Bus 7 to Downtown</a>
-		<a href='/sdnorth'>Bus 7 to Fiesole</a>
+		<a href='/sdsouth'>Bus 7 to Downtown</a><br />
+		<a href='/sdnorth'>Bus 7 to Fiesole</a><br />
 	</p>
 
 	<h2>Near EUI Flats</h2>
 	<p>
-		<a href='/flats'>Bus 1A From Salviati to S.M.N. Train Station</a>
+		<a href='/flats'>Bus 1A From Salviati to S.M.N. Train Station</a><br />
 	</p>
 	<h2>Piazza San Marco</h2>
 	<p>
-		<a href='/sm7'>Bus 7 to EUI and Fiesole</a>
-		<a href='/sm1'>Bus 1A/B to Lapo and Boccaccio (Alternate)</a>
+		<a href='/sm7'>Bus 7 to EUI and Fiesole</a><br />
+		<a href='/sm1'>Bus 1A/B to Lapo and Boccaccio (Alternate)</a><br />
 	</p>	
 
 	<h2>S.M.N. Train Station</h2>
 	<p>
-		<a href='/smn'>Bus 1A/B to Lapo and Boccaccio (Alternate)</a>
+		<a href='/smn'>Bus 1A/B to Lapo and Boccaccio (Alternate)</a><br />
 	</p>
 	<h2>Fiesole</h2>
 	<p>
 		<a href='/fiesole'>Bus 7 to San Domenico and San Marco</a>
 	</p>
 </body>
-</html>
+</html>"
 end
 
 get '/*' do
